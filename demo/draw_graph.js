c72e67214f1ca44a45d722ee4b8006f9a7aebd3e
Seigo Shigenaka <54699899+azsgws@users.noreply.github.com>
2019-09-25 13:33:04 +0900

    add files
    demo_sample.json is create_graph.py results.
    example.html is each node links.

---
+++ demo/draw_graph.js
@@ -0,0 +1,73 @@
+/*
+createGraph.pyで出力されたファイルとcytoscape.jsを使って
+グラフの描画を行う
+*/
+$(function(){
+    $.getJSON("./demo_sample.json", function(graph_data) {
+        //描画(graph_draw()をここに書き写す)
+        // cytoscapeグラフの作成(初期化)
+        var cy = cytoscape({
+            container: document.getElementById('demo'),
+            elements: [],
+
+            boxSelectionEnabled: true,
+            autounselectify: false,
+            selectionType: "additive"
+        });
+        // グラフにノードを追加
+        for(var data in graph_data["elements"]["nodes"]){
+            for(var component in graph_data["elements"]["nodes"][data]){
+                cy.add({
+                    group: "nodes",
+                    data:{
+                        id: graph_data["elements"]["nodes"][data][component]["id"],
+                        name: graph_data["elements"]["nodes"][data][component]["name"],
+                        dummy: graph_data["elements"]["nodes"][data][component]["dummy"],
+                        href: graph_data["elements"]["nodes"][data][component]["href"]
+                    },
+                    position:{
+                        x: graph_data["elements"]["nodes"][data][component]["x"] * 200,
+                        y: graph_data["elements"]["nodes"][data][component]["y"] * 200
+                    }
+                });
+            }
+        }
+        // グラフにエッジを追加
+        for(var data in graph_data["elements"]["edges"]){
+            for(var component in graph_data["elements"]["edges"][data]){
+                cy.add({
+                    group: "edges",
+                    data:{
+                        source: graph_data["elements"]["edges"][data][component]["source"],
+                        target: graph_data["elements"]["edges"][data][component]["target"]
+                    }
+                })
+            }
+        }
+        // グラフのスタイルを決定
+        cy.style([
+            {
+                selector: "node",
+                css: {"background-color": "red", "width": 50, "height": 50, "content": "data(id)"}
+            },
+            {
+                selector:"label",
+                css: {"text-halign":"center", "text-valign": "center"}
+            },
+            {
+                selector: "edge",
+                css: {"line-color": "black", "target-arrow-shape": "triangle", "curve-style": "straight",
+                "target-arrow-color": "black", "arrow-scale": 3, "width": 3}
+            },
+        ]);
+        // ノードをクリックした場合、リンクに飛ぶ(htmlリンクの設定)
+        cy.on("tap", "node", function(){
+            try {
+                window.open(this.data("href"));
+            } catch(e){
+                window.location.href = this.data("href");
+            }
+        });
+
+    });
+});
