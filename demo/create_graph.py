a4966dbc082553f0b92d11a6622277e7dbdec486
Seigo Shigenaka <54699899+azsgws@users.noreply.github.com>
2019-10-01 15:11:14 +0900

    Update create_graph.py
    Rewrite create_node_list().

--- demo/create_graph.py
+++ demo/create_graph.py
@@ -8,23 +8,6 @@
 import networkx as nx
 import json
 
-"""
-INPUT_NODE_DICT: 全ノードについての情報を辞書にまとめたもの。dict()
-    key: ノードの名前。
-    value: リスト
-        第1要素: keyのノードが指すノードの集合。set()
-        第2要素: keyのノードのリンク先URL。str()
-
-"""
-
-INPUT_NODE_DICT = {"f": [{"e", "b", "a"}, "example.html"], "g": [{"e"}, "example.html"],
-                   "h": [{"g", "f"}, "example.html"], "i": [{"a"}, "example.html"], "o": [{"m", "l"}, "example.html"],
-                   "p": [{"n", "k"}, "example.html"], "q": [{"k", "o", "i"}, "example.html"],
-                   "j": [{"i"}, "example.html"], "k": [{"j", "m"}, "example.html"], "l": [{"i", "a"}, "example.html"],
-                   "m": [{"i"}, "example.html"], "n": [{"j", "m"}, "example.html"], "a": [set(), "example.html"],
-                   "b": [{"a"}, "example.html"], "c": [{"b", "e"}, "example.html"], "d": [{"c", "a"}, "example.html"],
-                   "e": [{"a"}, "example.html"]}
-
 
 class Node:
     """
@@ -58,140 +41,43 @@ class Node:
 
 def create_node_list():
     """
-    INPUT_NODE_DICTをNodeクラスでインスタンス化したものをリストにまとめる。
+    input_node_dictをNodeクラスでインスタンス化したものをリストにまとめる。
     各属性には次の物を格納する。
-        ・name:  INPUT_NODE_DICTのkey。str。
-        ・target_nodes: INPUT_NODE_DICTのvalueの第一要素。set()。
-        ・source_nodes: INPUT_NODE_DICTを用いてsourceとなるノードを検索し、まとめた集合source_nodes。set()。
+        ・name:  input_node_dictのkey。str。
+        ・target_nodes: input_node_dictのvalueの第一要素。set()。
+        ・source_nodes: target_nodesをもとに作成したsource_nodes。set()。
         ・x, y: -1。int。
         ・href: INPUT_NODE_DICTのvalueの第二要素。str。
         ・is_dummy: False。bool。
 
     Args:
-        INPUT_NODE_DICT（global）
+        input_node_dict: 入力されたノードの関係を示す辞書型データ。
+                         ノードの名前をキーに持ち、値としてリストを持つ。リストの要素は次のようになる。
+                             第1要素: keyのノードが指すノードの集合。set()
+                             第2要素: keyのノードのリンク先URL。str()   
+        
     Returns:
         インスタンス化されたノードのリスト。
     """
-    node_data_list = []
-    for node_name, node_data in INPUT_NODE_DICT.items():
-        # source_nodesの作成
-        source_nodes = set()
-        for source, source_data in INPUT_NODE_DICT.items():
-            for target_node in source_data[0]:
-                if target_node == node_name:
-                    source_nodes.add(source)
-        # node_date_listの作成
-        n = Node(node_name, node_data[0], source_nodes, -1, -1, node_data[1], False)
-        node_data_list.append(n)
-    return node_data_list
-
-
-def create_dependence_graph(graph, node_list):
-    """
-    依存関係を示すグラフを作成する。
-
-    Args:
-        graph:操作するグラフ。
-        node_list:全ノードをNodeクラスでまとめたリスト。
-
-    Return:
-    """
-    for source_node in node_list:
-        graph.add_node(source_node.name)
-        for target_node in source_node.target_nodes:
-            graph.add_node(target_node)
-            graph.add_edge(source_node.name, target_node)
-
-
-"""
-#1．階層割当(最長パス法)
-"""
-
-
-def assign_top_node(node_list):
-    """
-    グラフのルートを決定する。ルートは矢印が出ていない(参照をしていない)ノードとなる。
-　　その後、level2node()でその下の階層のノードを決めていく。
-
-    Args:
-        node_list:全ノードをNodeクラスでまとめたリスト。
-
-    Return:
-
-    """
-    for top_node in node_list:
-        if set() == top_node.target_nodes:
-            top_node.y = 0
-            top_node.x = 0
-            assign_level2node_recursion(node_list, top_node, 0)
-
-
-def assign_level2node_recursion(node_list, target, target_level):
-    """
-    階層が1以上（y座標が1以上）のノードの階層を再帰的に決定する。階層の割当は次のルールに従う。
-    ・まだ階層を割り当てていないノードならば、targetの1つ下の階層に割り当てる。
-    ・既に座標を割り当てており、その階層が今の階層(source_node_level)以上高い階層ならば、一つ下の階層に再割当する。
-　　・既に階層を割り当てており、その階層が今の階層よりも低い階層ならば、何もしない。
-
-    Args:
-        node_list:全ノードをNodeクラスでまとめたリスト。
-        target: ターゲットとなるノード。このノードを指すノードに階層を割り当てていく。
-        target_level: targetの階層。targetを指すノードは基本的にこの階層の1つ下の階層に割り当てられる。
-    """
-    assign_node_level = target_level + 1
-    for assign_node_name in target.source_nodes:
-        assign_node = [source for source in node_list if source.name == assign_node_name][0]
-        if assign_node.x < 0:
-            assign_node.y = assign_node_level
-            assign_node.x = 0
-            assign_level2node_recursion(node_list, assign_node, assign_node_level)
-        if assign_node.x > -1 and assign_node.y <= assign_node_level:
-            assign_node.y = assign_node_level
-        else:
-            pass
-
-
-def assign_x_coordinate(node_list):
-    """
-    全てのノードに対して、x座標を割り当てる。
-
-    Args:
-        node_list:全ノードをNodeクラスでまとめたリスト。
-
-    Return:
-    """
-    below_level = max([node.y for node in node_list])
-    assigning_level = 0
-    while assigning_level <= below_level:
-        x_coordinate = 0
-        for equal_level_node in [node for node in node_list if node.y == assigning_level]:
-            equal_level_node.x = x_coordinate
-            x_coordinate += 1
-        assigning_level += 1
-
-
-def node_list2node_dict(node_list):
-    """
-    ノードについての情報（属性）をリスト形式から辞書形式に変換する。
-
-    Args:
-        node_list:全ノードをNodeクラスでまとめたリスト。
-
-    Return:
-        各ノードのname, href, x, y, is_dummyを持つ辞書。
-        キーはnameで、その値としてhref, x, y, is_dummyをキーに持つ辞書が与えられる。
-        例:
-        node_dict = {"f": { "href": "example.html", "x": 0, "y": 2, "is_dummy": false}, ... }
-    """
+    node_list = []
     node_dict = {}
-    for node in node_list:
-        node_name = str(node.name)
-        node_dict[node_name] = {}
-        node_dict[node_name]["href"] = str(node.href)
-        node_dict[node_name]["x"] = int(node.x)
-        node_dict[node_name]["y"] = int(node.y)
-        node_dict[node_name]["is_dummy"] = bool(node.is_dummy)
-    return node_dict
+    # node_dict, node_listの作成
+    for k, v in input_node_dict.items():
+        n = Node(k, set(), set(), -1, -1, v[1], False)
+        node_dict[k] = n
+        node_list.append(n)
+        
+    # target_nodesの作成
+    for k, v in input_node_dict.items():
+        for target in v[0]:
+            node_dict[k].target_nodes.add(node_dict[target])
+            
+    # source_nodesの作成
+    for k, v in node_dict.items():
+        for target in v.target_nodes:
+            target.source_nodes.add(node_dict[k])
+
+    return node_list
 
 
 def main():
@@ -200,27 +86,57 @@ def main():
 
     Return:
     """
-    node_list = create_node_list()
+    import random
 
-    # 有向グラフGraphの作成
-    graph = nx.DiGraph()
+    def shuffle_dict(d):
+        """
+        辞書（のキー）の順番をランダムにする
 
-    create_dependence_graph(graph, node_list)
-    assign_top_node(node_list)
-    assign_x_coordinate(node_list)
-    node_attributes = node_list2node_dict(node_list)
+        Args:
+            d: 順番をランダムにしたい辞書。
 
-    # nodes_attrsを用いて各ノードの属性値を設定
-    nx.set_node_attributes(graph, node_attributes)
+        Return:
+            dの順番をランダムにしたもの
+        """
+        keys = list(d.keys())
+        random.shuffle(keys)
+        [(key, d[key]) for key in keys]
+        random.shuffle(keys)
+        [(key, d[key]) for key in keys]
+        random.shuffle(keys)
+        keys = [(key, d[key]) for key in keys]
+        return dict(keys)
 
-    # グラフの描画
-    nx.draw_networkx(graph)
+    """
+       input_node_dict: 全ノードについての情報を辞書にまとめたもの。dict()
+           key: ノードの名前。
+           value: リスト
+               第1要素: keyのノードが指すノードの集合。set()
+               第2要素: keyのノードのリンク先URL。str()
 
-    # cytoscape.jsの記述形式(JSON)でグラフを記述
-    graph_json = nx.cytoscape_data(graph, attrs=None)
+    """
+    input_node_dict = {"a": [set(), "example.html"],
+                       "b": [{"a"}, "example.html"],
+                       "c": [{"b", "e"}, "example.html"],
+                       "d": [{"c", "a"}, "example.html"],
+                       "e": [{"a"}, "example.html"],
+                       "f": [{"e", "b", "a"}, "example.html"],
+                       "g": [{"e"}, "example.html"],
+                       "h": [{"g", "f"}, "example.html"],
+                       "i": [{"a"}, "example.html"],
+                       "j": [{"i"}, "example.html"],
+                       "k": [{"j", "m"}, "example.html"],
+                       "l": [{"i", "a"}, "example.html"],
+                       "m": [{"i"}, "example.html"],
+                       "n": [{"j", "m"}, "example.html"],
+                       "o": [{"m", "l"}, "example.html"],
+                       "p": [{"n", "k"}, "example.html"],
+                       "q": [{"k", "o", "i"}, "example.html"],
+                       }
 
-    with open('demo_sample.json', 'w') as f:
-        f.write(json.dumps(graph_json))
+    node_list = create_node_list(shuffle_dict(input_node_dict))
 
-
-main()
+    
+if __name__ == "__main__":
+    main()
+    
